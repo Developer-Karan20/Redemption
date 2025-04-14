1c5455eabc77b9295c29f58b02b343c8cbc2102c
Developer-Karan20 <kg2310957@gmail.com>
2025-04-14 13:09:17 +0000

    learned about conversion of different dataTypes

---
+++ 01_basics/03_conversion.operations.js
@@ -0,0 +1,33 @@
+let score = "Karan"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => i; false => 0
+
+let isLoggedIn = "Karan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "karan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+// ****************************Operations********************************
