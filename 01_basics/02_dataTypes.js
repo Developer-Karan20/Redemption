b47a90cbc56ed05df8c2dabdb75c05994fa96454
Developer-Karan20 <kg2310957@gmail.com>
2025-04-14 12:28:34 +0000

    started learning dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code redability should be high
+
+console.log("Karan");
+
+let name = "Karan"
+let age = 21;
+let isLoggedIn = false
+
+
+// number => 2 to the power 53
+// bigint => very large number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declare kiya hai par value nahi declare kari huwi hai
+// symbol => used for unique
+
+
+// object
+
+console.log(typeof "hitesh"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
