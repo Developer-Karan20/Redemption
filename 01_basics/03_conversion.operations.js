e642fe5fbdfc9e463944d4555a91dbd2e6afa004
Developer-Karan20 <kg2310957@gmail.com>
2025-04-15 15:41:07 +0000

    did learn about confusion of dataTypes in js

--- 01_basics/03_conversion.operations.js
+++ 01_basics/03_conversion.operations.js
@@ -31,3 +31,33 @@ console.log(typeof stringNumber);
 
 
 // ****************************Operations********************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Karan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);  
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// ALL THE CODE WRITTEN ABOVE IS WRONG FORMAT.
+
+console.log( (3 + 4) * 5 % 3); // this is the correct way...use parenthesis in between of operations. 
+
+// here are some common errors again that devs do
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// study about this in mdn docs.
+
