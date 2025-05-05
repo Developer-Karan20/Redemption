3561587aa817ae7838d1cbcf8e9c35f4bcf53bb6
Developer-Karan20 <kg2310957@gmail.com>
2025-05-05 16:56:27 +0000

    started learning about functions.

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function sayMyName() {
+   console.log("K");
+   console.log("A");
+   console.log("R");
+   console.log("A");
+   console.log("N");
+}
+
+//agar mai yha par bass **sayMyName** likhu to wo hoga bss funtion ka reference lekin jab mai likhta hu **sayMyName()** ye hota hai execution karwana. 
+sayMyName();
+
+
+// functions mein jab declare kar rahe hote hai to uske andar jo likhte hai use kehte hai parametres. aur call karte time jo value enter kar rahe hote hai usko bolte hai arguments. 
+                    //parametres
+function addTwoNum(number1, number2) {
+    console.log(number1 + number2);
+    
+}
+        //Arguments.    
+addTwoNum(2, 99); 
