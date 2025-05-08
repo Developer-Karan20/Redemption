2e8f78d06c1dfea63d32dcc8412a86e39b955897
Developer-Karan20 <kg2310957@gmail.com>
2025-05-08 02:42:35 +0000

    did learn about scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,7 +7,7 @@ var c = 30;
 
 /* now thoda local aur global scope ke baare mein bhi discuss kar lete hai.. 
 
-Local/block scope -> local scope matlab kisi ek function ya if else ke andar kisi varibale ko declare kiya to wo local scope hoga jaise. ye : 
+Local/block scope -> local scope matlab kisi ek function ya if else ke andar kisi varibale ko declare kiya to wo local scope hoga jaise ye : 
 
 if(true) {
   const name = "Karan"
@@ -23,3 +23,52 @@ if(true) {
 }
 
 console.log("INNER: ", globalScopeNO);
+
+
+// Nested scope : isko aise samjhenge ki jaise chote bacche bado se icecream maang sakte hai ya cheen sakte hai. Par agar bade chote baccho se icecream mange to kharab lagta hai. Theek aise hee local/block scope ke andar global scope ke variable ko access kiya ja sakta hai par global scope mein local/block scope ke varibale ko access nahi kiya jaa sakta. 
+
+function one() {
+  const username = "Karan";
+  // console.log(username);
+
+  function two() {
+    const website = "Youtube";
+    console.log(username);
+  }
+
+  // console.log(website);
+
+
+// two(); // wxpected output : Karan cuz console statement got executed. 
+
+}
+
+one(); // expected output : empty cuz execute to huwa but console statement nahi thi to display kuch bhi nahi huwa. 
+
+
+
+//++++++++++++++++++++ interesting ++++++++++++++++
+
+/* function declare karne ke kaafi methods hote hai unme se 2 hai Ek to Normal wala aur ek hota hai function ko variable ke andar store karna, jisko expression bhi kehte hai. */
+
+// Here fact of the matter is: hum addone() yani function ko normal method ke andar initialization se pehle hee call kar sakte hai. 
+
+console.log(addone(5));
+
+function addone(num) {
+  return num+1;
+}
+
+// addone();
+
+
+
+// Par agar expression mein wo karne jaaye to error de dega. 
+
+console.log(addtwo(4));
+
+const addtwo = function(num) {
+  return num+2;
+}
+
+// addtwo();
