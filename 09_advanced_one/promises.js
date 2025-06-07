8e98403304794e86e7317dec52a84b9935c86231
Developer-Karan20 <kg2310957@gmail.com>
2025-06-07 06:28:20 +0000

    did learn about fetch method and .then .catch and all that bullshit.

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -41,4 +41,77 @@ promiseThree.then(function(user) {
 });
 
 
- 
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        let error = true;
+        if(!error) {
+            resolve({userName: "karan", password: "123"});
+        } else {
+            reject("ERROR: Something went wrong");
+        }
+    }, 1000);
+});
+
+promiseFour.then( (user) => {
+   console.log(user);
+   return user.userName;
+}).then((username) => {
+    console.log(username);
+}).catch(function(error) {
+    console.log(error);
+}).finally(() => {
+    console.log("The promise is either resolved or rejected");
+});
+//finally to use hoga hee hoga, it is default. 
+
+
+
+//THE ASYNC AWAIT APPROACH : thoda der wait karna padta hai kaam hone ke liye.
+const promiseFive = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        let error = true;
+        if(!error) {
+            resolve({userName: "javascript", password: "123"});
+        } else {
+            reject("ERROR: JS went wrong");
+        }
+    }, 1000);
+});
+
+async function consumePromiseFive() {
+   try {
+    const response = await promiseFive; //jo bhi promiseFive se aaye...ab chahe wo resolve hoke aaye ya reject hoke aaye. mai uska wait kar rha hu. 
+    console.log(response);
+   } catch (error) {
+    console.log(error); //gracefully handled error. 
+   }
+}
+consumePromiseFive();
+
+
+//using async await and fetch() method.
+async function getAllUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        const data = await response.json(); //we have used await here because it takes time to convert data into a json file.
+        console.log(data);
+    }catch (error) {
+        console.log("E: ", error);
+    }
+}
+
+getAllUsers();
+
+
+
+//using .then() and .catch() 
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data); //response handle karne ke liye..thenable bolte hai iss process ko. data likhna jaroori nahi hai kuch bhi likh sakte hai. 
+})
+.catch((error) => console.log(error)) //can write in single line. 
