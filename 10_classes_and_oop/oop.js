e527ee49a69ef1de5c21f305c886de4c11714bca
Developer-Karan20 <kg2310957@gmail.com>
2025-06-09 13:34:27 +0000

    hahahhahaa

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -8,9 +8,21 @@ const user = {
     signedIn: true,
     //iske andar functions bhi diye jaate hai.
     getUserDetails: function() {
-        console.log("got user details from database");
+        // console.log("got user details from database");
+        console.log(`Username: ${this.userName}`); //this mtlb mai apne saman ki baat kar rha hu. 
+        console.log(this); // will print current context....that literal object. 
     }
+
 }
 
 console.log(user.userName);
 console.log(user.getUserDetails()); //method hai to run karana padega khali reference nahi de sakte. 
+
+console.log(this); //node environment mein empty object milta hai...par browser mein window object milta hai jisme bhar bhar ke methods hote hai aur baaki cheeze bhi hoti hai. 
+
+
+//CONSTRUCTOR FUNCTIONS
+
+function User(username, logincount, isloggedin) {
+    this.username = userName,
+}
