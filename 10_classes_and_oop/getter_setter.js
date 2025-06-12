4e77dea212db2625e1abcf34861f23fd41f3da00
Developer-Karan20 <kg2310957@gmail.com>
2025-06-12 12:34:12 +0000

    did learn about getter and setter in different sceniorios

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,4 +1,6 @@
 //By using getter and setter we can actually control what other people see and modify. Like hum ek class banate hai to uske andar jo bhi properties hai unko set se modify karke get se ek filtered version jo ki humne hee banaya hai wo user ko dikha sakte hai. original value to rhegi par modified value bahar waalo ko dikhegi. 
+// 
+// different variable isliye assign karne padye hai kyuki call stack aur get set mein race lag jaati hai....kon pehle execute karega..jiski wajah se stack overflow jo jaaya hai...isliye slightly change karke linkhenge to get set hee final execution karte hai. 
 
 
 class User {
@@ -26,7 +28,7 @@ class User {
 
 
 
-//MORE SECURE WAY.
+//MORE SECURE WAY. Or this is the way jisse real mein koi access nahi kar sakta...uper waale se access kar sakte hai par ye laga liya to guru code ekdum unbreakable ban jayega. 
 const karan = new User("karan@k.ai", "abc@kgf");
 console.log(karan.password);
 console.log(karan.email);
